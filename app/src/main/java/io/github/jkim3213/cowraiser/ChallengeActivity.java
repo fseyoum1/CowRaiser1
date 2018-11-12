4377742c0aad46a800b9e655e8d212953a5b52b9
Jinwoo Kim <jinwookim3213@outlook.com>
2018-11-12 13:16:14 -0500

    put log in abstract class

--- app/src/main/java/io/github/jkim3213/cowraiser/ChallengeActivity.java
+++ app/src/main/java/io/github/jkim3213/cowraiser/ChallengeActivity.java
@@ -2,14 +2,18 @@ package io.github.jkim3213.cowraiser;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public abstract class ChallengeActivity extends AppCompatActivity {
 
     protected TextView challengeTitle;
     protected TextView challengeDesc;
     protected Button logButton;
+    protected int ecoDollars = 0;
+    protected int carbonLbs = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public abstract class ChallengeActivity extends AppCompatActivity {
         challengeTitle = findViewById(R.id.challengeTitle);
         challengeDesc = findViewById(R.id.challengeDesc);
         logButton = findViewById(R.id.logButton);
+        logButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                UserProfile.carbonLbs += carbonLbs;
+                UserProfile.ecoDollars += ecoDollars;
+                String complete = getString(R.string.challenge_complete_toast, ecoDollars);
+                Toast.makeText(getApplicationContext(), complete, Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
 
     }
 
